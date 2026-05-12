8a2d27a44f5c756fe94969906147ea0f2c7ae391
J <42168680+jtejolan@users.noreply.github.com>
2026-05-12 15:04:30 -0400

    Update get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,7 @@
+import os
+
 def get_files_info(working_directory, directory="."):
+  work_dir = os.path.abspath(working_directory)
+  target_dir = os.path.normpath(os.path.join(work_dir, directory))
+  
+  os.path.commonpath([work_dir, target_dir])
