645c285f7c0b3cdc654a6b5a65c73464e9e9ba74
J <42168680+jtejolan@users.noreply.github.com>
2026-05-12 16:20:39 -0400

    Enhance get_files_info with error handling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,17 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-  work_dir = os.path.abspath(working_directory)
-  target_dir = os.path.normpath(os.path.join(work_dir, directory))
-  
-  os.path.commonpath([work_dir, target_dir])
+    try:
+        work_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(work_dir, directory))
+        valid_target_dir = os.path.commonpath([work_dir, target_dir]) == work_dir
+
+        if not valid_target_dir:
+                return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        elif not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        else:
+            return f'Success: "{directory}" is within the working directory'
+        
+    except Exception as e:
+        return f"Error: {e}"
