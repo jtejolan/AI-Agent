73ffeb29244bedc5195e8f3f66c2901d81732dc1
jtejolan <thrills1.leis@icloud.com>
2026-05-13 21:13:53 -0400

    Run Python File Complete

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,5 @@
 import os
-from config import *
+import subprocess
 
 def run_python_file(working_directory, file_path, args=None):
     try:
@@ -7,7 +7,7 @@ def run_python_file(working_directory, file_path, args=None):
         target_file_path = os.path.normpath(os.path.join(work_dir, file_path))
         valid_target_file = os.path.commonpath([work_dir, target_file_path]) == work_dir
 
-        # VALIDATION BLOCK
+        ## VALIDATION BLOCK##
         if not valid_target_file:
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(target_file_path):
@@ -15,8 +15,36 @@ def run_python_file(working_directory, file_path, args=None):
         if not file_path.endswith('.py'):
             return f'Error: "{file_path}" is not a Python file'
 
-        command = ["python", absolute_file_path]
+        ## ACTION BLOCK##
+        command = ["python", target_file_path]
+        if args is not None:
+            command.extend(args)
         
+        result = subprocess.run(
+            command,
+            cwd=work_dir,
+            capture_output = True,
+            text = True,
+            timeout = 30
+            
+        )
+        
+        output = ""
+
+        if result.returncode != 0:
+            output += f"Process exited with code {result.returncode}\n"
+
+        if result.stdout:
+            output += f"STDOUT:\n{result.stdout}\n"
+
+        if result.stderr:
+            output += f"STDERR:\n{result.stderr}\n"
+
+        if not result.stdout and not result.stderr:
+            output += "No output produced"
+
+        return output
+
 
     except Exception as e:
-        return f"Error: {e}"
+        return f"Error: executing Python file: {e}"
